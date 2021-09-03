75cb33f978949b3deec6896923b27e9fd5a1220b
wandercosta <aquinowander@hotmail.com>
2021-09-03 17:37:51 -0300

    Teste Commit por Bash

---
+++ basico/comentarios.php
@@ -0,0 +1,27 @@
+<div class="titulo">Comentários PHP</div>
+
+
+<?php 
+ echo "<h1 style='font-size:30px;'>COMENTARIOS LINHA PHP</h1>";
+
+echo'<div style="font-size:20px;">
+// echo "COMENTARIO PHP"; <br>
+#echo "COMENTARIO PHP";   <br>
+#echo  //"COMENTARIO PHP";<br>
+//echo #"COMENTARIO PHP"; <br>
+//?></div>';?>
+
+<?php 
+ echo "<h1 style='font-size:30px;'>COMENTARIOS DE BLOCO</h1>";
+ echo'<div style="font-size:20px;">
+/*
+    linha 1 blablabla <br>
+    linha 2 blablabla <br>
+    linha 3 blablabla <br>
+    linha 4 blablabla <br>
+
+    ?>  
+*/
+</div>';?>
+
+
